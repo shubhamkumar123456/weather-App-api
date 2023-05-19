eed48464e40c6f7ab9f8846cae61cb8c984376c0
shubham kumar <107460088+shubhamkumar123456@users.noreply.github.com>
2023-05-19 09:01:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const app = express();
-require("dotenv").config();
-app.use(express.json());
-const port=3001;
+const rateLimit= require('express-rate-limit');
 
 const forcastRoute = require("./routes/forecast");
 const currentWeatherRoute = require("./routes/currentWeather");
+
+const app = express();
+app.use(express.json());
+require("dotenv").config();
+
+const port = 3000;
+
+// Global middleware
+const limiter=rateLimit({
+  max:60,
+  windowMs:60*60*1000,
+  message:"Too many requests from this Ip, please try again in an hour"
+})
+app.use('/',limiter)
 
 // Routes
 app.use("/weatherForcast", forcastRoute);
